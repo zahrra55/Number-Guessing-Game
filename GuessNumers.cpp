8070cad82696a9432cd47bf8ac1c324447a8679e
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-28 17:30:39 +0300

    Update GuessNumers.cpp

--- GuessNumers.cpp
+++ GuessNumers.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+
 using namespace std;
+
 int main() {
-    int RightNo = 87;
+    srand(static_cast<unsigned>(time(0))); // Seed for random number generation
+    int RightNo = rand() % 100 + 1;       // Random number between 1 and 100
     int GussNo;
-    cout<< "Guess the number!  ";
 
-    while(true){
-    cin>>GussNo;
-    if(GussNo==RightNo)
-    {
-    cout<<"Correct! Well done."<<endl;
-    break;
-    }
-    else if(GussNo>RightNo)
-        cout<<"the Number is Smaller! try again.."<<endl;
-    else
-        cout<<"the Number is Bigger! try again.."<<endl;
+    cout << "Guess the number (between 1 and 100): ";
+
+    while (true) {
+        if (!(cin >> GussNo)) {
+            cout << "Please enter a valid number!" << endl;
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            continue;
+        }
+
+        if (GussNo == RightNo) {
+            cout << "Correct! Well done." << endl;
+            break;
+        } else if (GussNo > RightNo) {
+            cout << "The number is smaller! Try again..." << endl;
+        } else {
+            cout << "The number is bigger! Try again..." << endl;
+        }
     }
 
-system("pause");
+    cout << "Thanks for playing! Press Enter to exit...";
+    cin.ignore();
+    cin.get();
     return 0;
 }
